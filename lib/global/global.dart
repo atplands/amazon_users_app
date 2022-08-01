84a02e8ff64d5be9bc964a347a33ff6938bec25b
FlutLab.io <>
2022-08-01 20:08:11 +0000

    Initial commit

---
+++ lib/global/global.dart
@@ -0,0 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:users_app/assistantMethods/cart_methods.dart';
+
+SharedPreferences? sharedPreferences;
+CartMethods cartMethods = CartMethods();
+String previousEarning = "";
